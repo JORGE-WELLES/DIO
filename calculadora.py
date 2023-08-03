f0d3c7881a44b387b106c92d86e6df83a76b0696
JORGE-WELLES <welles0311@gmail.com>
2023-08-03 15:24:30 -0300

    Desafio Calculadora com Menu

--- calculadora.py
+++ calculadora.py
@@ -1,14 +1,54 @@
+def calculadora(num1, num2, op):
+    
+    
+        if op == 1:
+            resp = num1 + num2
+            opera = " + "
+            return print(str(num1) + opera + str(num2) + " = " + str(resp))
+            
+        elif op == 2:
+                
+                resp = num1 - num2
+                opera = " - "
+                return print(str(num1) + opera + str(num2) + " = " + str(resp))
+        elif op == 3:
+                
+                resp = num1 * num2
+                opera = " X "
+                return print(str(num1) + opera + str(num2) + " = " + str(resp))
+        elif op == 4:
+                
+                resp = num1 / num2
+                opera = " ÷ "
+                return print(str(num1) + opera + str(num2) + " = " + str(resp))
+        
+               
+       
 
-def calculadoram(num1, num2, oper):
-    if oper == 1:
-        resultado = num1 + num2
-    elif oper == 2 :
-        resultado = num1 - num2
-    elif oper == 3 :
-        resultado = num1 * num2
-    elif oper == 4 :
-        resultado = num1 / num2
-    return resultado
+print("""
+Escolha uma Opção:
+           1 = Soma
+           2 = Subtração
+           3 = Multiplicação
+           4 = Divisão
+           0 = Sair
+      
+ """)
 
-calculo = calculadoram(10, 2, 4)
-print(calculo)
+
+
+op = int(input("Escolha uma Opção: "))
+if op != 0:
+   num1 = int(input("Qual o primeiro valor "))
+   num2 = int(input("Qual o segundo valor "))
+
+while op != 0:
+       calculadora(num1, num2, op)
+
+       op = int(input("Escolha uma Opção: "))
+       if op != 0:
+          num1 = int(input("Qual o primeiro valor "))
+          num2 = int(input("Qual o segundo valor "))
+       
+
+
